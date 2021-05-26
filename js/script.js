68d557e49a893f193c5778e5861a73b1dc7e8ca1
IrynaSolonenko <iryna17z89@gmail.com>
2021-05-26 23:01:43 +0300

    confirm

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let admin;
+admin = 'John';
+
+let name;
+name = 'John';
+
+
+alert(admin);
+alert(name);
+
+//--------------------------------------------------
+
+let EarthName;
+EarthName = 'Planet Earth';
+
+let onlineUserName;
+userName = 'Pavel';
+
+//-----------------------------------------------------
+
+let str;
+str = '10';
+console.log(typeof str, str);
+
+let n;
+n = 10;
+console.log(typeof n, n);
